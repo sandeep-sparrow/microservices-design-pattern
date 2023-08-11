cfe08f23e36d83beb7a2e31935adf19dfaf24c46
Sandeep Prajapati <sandeep.p4856@gmail.com>
2023-08-11 17:40:30 +0530

    added Java Bean Validation - using spring-boot-starter-validation dependency

--- ProductService/ProductService/src/main/java/com/appsdeveloperblog/estore/ProductService/command/rest/ProductCommandController.java
+++ ProductService/ProductService/src/main/java/com/appsdeveloperblog/estore/ProductService/command/rest/ProductCommandController.java
@@ -2,6 +2,8 @@ package com.appsdeveloperblog.estore.ProductService.command.rest;
 
 import java.util.UUID;
 
+import javax.validation.Valid;
+
 import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
@@ -29,7 +31,7 @@ public class ProductCommandController {
 	}
 	
 	@PostMapping
-	public String createProduct(@RequestBody CreateProductRestModel createProductRestModel) {
+	public String createProduct(@Valid @RequestBody CreateProductRestModel createProductRestModel) {
 		
 		CreateProductCommand createProductCommand = CreateProductCommand.builder()
 		.price(createProductRestModel.getPrice())
